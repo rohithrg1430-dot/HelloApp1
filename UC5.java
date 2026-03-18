2dfe27cb18e6b65207ea7d2c77e24bda89649749
rohithrg1430-dot <rohithrg1430@gmail.com>
2026-03-18 13:49:02 +0530

    Create UC5.java

---
+++ UC5.java
@@ -0,0 +1,15 @@
+public class UC5 {
+
+public static void main(String[] args) {
+
+    if (args.length > 0) {
+        for (String name : args) {
+            System.out.println("Hello " + name);
+        }
+    } else {
+        System.out.println("Hello World");
+    }
+
+}
+
+}
